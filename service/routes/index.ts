9b7ccc9c7a44b802e403aaa3da50c9de84e2c7a4
Ibrahim <ikadismile@gmail.com>
2022-10-10 21:32:47 +0000

    added typescript and other setup files

---
+++ service/routes/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+// Health check route
+router.get('/api/ping', function (req, res, next) {
+  res.sendStatus(200);
+});
+
+module.exports = router;
